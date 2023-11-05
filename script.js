14dc18a5222620c7b3c8a725536dcd2199d229e2
SORRYMAKER100 <91243357+SORRYMAKER100@users.noreply.github.com>
2023-11-05 14:25:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,150 +1,9 @@
-//let map;
-//let directionsService;
-//let directionsRenderer;
-//let autocompleteStart;
-//let autocompleteEnd;
-//let panorama;
-//
-//function initMapAndAutocomplete() {
-//    map = new google.maps.Map(document.getElementById('map'), {
-//        center: {lat: -34.397, lng: 150.644},
-//        zoom: 8
-//    });
-//
-//    directionsService = new google.maps.DirectionsService();
-//    directionsRenderer = new google.maps.DirectionsRenderer();
-//    directionsRenderer.setMap(map);
-//
-//    panorama = new google.maps.StreetViewPanorama(
-//        document.getElementById('pano'), {
-//            position: {lat: -34.397, lng: 150.644},
-//            pov: {
-//                heading: 34,
-//                pitch: 10
-//            }
-//        });
-//    map.setStreetView(panorama);
-//
-//    autocompleteStart = new google.maps.places.Autocomplete(
-//        document.getElementById('start'),
-//        { types: ['geocode'] }
-//    );
-//    autocompleteEnd = new google.maps.places.Autocomplete(
-//        document.getElementById('end'),
-//        { types: ['geocode'] }
-//    );
-//}
-//
-//function planRoute() {
-//    let startAddr = document.getElementById('start').value;
-//    let endAddr = document.getElementById('end').value;
-//    let selectedMode = document.getElementById('mode').value;
-//
-//    directionsService.route({
-//        origin: startAddr,
-//        destination: endAddr,
-//        travelMode: google.maps.TravelMode[selectedMode]
-//    }, function(response, status) {
-//        if (status === 'OK') {
-//            directionsRenderer.setDirections(response);
-//            const endPoint = response.routes[0].legs[0].end_location;
-//            showStreetView(endPoint);
-//        } else {
-//            window.alert('Directions request failed due to ' + status);
-//        }
-//    });
-//}
-//
-//
-//function showStreetView(position) {
-//    panorama.setPosition(position);
-//    panorama.setVisible(true);
-//}
-//let map, pano, directionsRenderer, directionsService;
-//
-//function initMapAndAutocomplete() {
-//    directionsRenderer = new google.maps.DirectionsRenderer();
-//    directionsService = new google.maps.DirectionsService();
-//
-//    map = new google.maps.Map(document.getElementById('map'), {
-//        center: { lat: -34.397, lng: 150.644 },
-//        zoom: 6
-//    });
-//
-//    pano = new google.maps.StreetViewPanorama(
-//        document.getElementById('pano'), {
-//            position: { lat: -34.397, lng: 150.644 },
-//            pov: { heading: 34, pitch: 10 }
-//        }
-//    );
-//
-//    directionsRenderer.setMap(map);
-//    directionsRenderer.setPanel(document.getElementById('right-panel'));
-//
-//    const control = document.getElementById('floating-panel');
-//    control.style.display = 'block';
-//    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-//}
-//
-//function planRoute() {
-//    let startAddr = document.getElementById('start').value;
-//    let endAddr = document.getElementById('end').value;
-//    let selectedMode = document.getElementById('mode').value;
-//
-//    directionsService.route({
-//        origin: startAddr,
-//        destination: endAddr,
-//        travelMode: google.maps.TravelMode[selectedMode]
-//    }, function(response, status) {
-//        if (status === 'OK') {
-//            directionsRenderer.setDirections(response);
-//            const endPoint = response.routes[0].legs[0].end_location;
-//            showStreetView(endPoint);
-//        } else {
-//            window.alert('Directions request failed due to ' + status);
-//        }
-//    });
-//}
-//
-//function showStreetView(location) {
-//    pano.setPosition(location);
-//    pano.setPov({ heading: 34, pitch: 10 });
-//}
-//
-//function useCurrentLocation() {
-//    if (navigator.geolocation) {
-//        navigator.geolocation.getCurrentPosition(function(position) {
-//            let pos = {
-//                lat: position.coords.latitude,
-//                lng: position.coords.longitude
-//            };
-//
-//            map.setCenter(pos);
-//            pano.setPosition(pos);
-//            document.getElementById('start').value = pos.lat + ',' + pos.lng;
-//        }, function() {
-//            handleLocationError(true, map.getCenter());
-//        });
-//    } else {
-//        // Browser doesn't support Geolocation
-//        handleLocationError(false, map.getCenter());
-//    }
-//}
-//
-//function handleLocationError(browserHasGeolocation, pos) {
-//    window.alert(browserHasGeolocation ?
-//                'Error: The Geolocation service failed.' :
-//                'Error: Your browser doesn\'t support geolocation.');
-//}
-let map, pano, directionsRenderer, directionsService;
+let map, pano, directionsRenderer, directionsService, autocompleteStart, autocompleteEnd, placesService;
 
 function initMapAndAutocomplete() {
-    directionsRenderer = new google.maps.DirectionsRenderer();
-    directionsService = new google.maps.DirectionsService();
-
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 6
+        zoom: 8
     });
 
     pano = new google.maps.StreetViewPanorama(
@@ -154,24 +13,25 @@ function initMapAndAutocomplete() {
         }
     );
 
+    directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsService = new google.maps.DirectionsService();
     directionsRenderer.setMap(map);
     directionsRenderer.setPanel(document.getElementById('right-panel'));
 
-    // 地址自动补全
-    new google.maps.places.Autocomplete(document.getElementById('start'));
-    new google.maps.places.Autocomplete(document.getElementById('end'));
+    autocompleteStart = new google.maps.places.Autocomplete(document.getElementById('start'));
+    autocompleteEnd = new google.maps.places.Autocomplete(document.getElementById('end'));
+    placesService = new google.maps.places.PlacesService(map);
 
-    const control = document.getElementById('floating-panel');
-    if (control) {
-        control.style.display = 'block';
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
-    }
+    autocompleteStart.bindTo('bounds', map);
+    autocompleteEnd.bindTo('bounds', map);
 }
 
 function planRoute() {
     let startAddr = document.getElementById('start').value;
     let endAddr = document.getElementById('end').value;
     let selectedMode = document.getElementById('mode').value;
+    let radiusInKm = document.getElementById('radius').value || '10';
+    let searchRadius = parseInt(radiusInKm) * 1000;
 
     directionsService.route({
         origin: startAddr,
@@ -182,6 +42,7 @@ function planRoute() {
             directionsRenderer.setDirections(response);
             const endPoint = response.routes[0].legs[0].end_location;
             showStreetView(endPoint);
+            showRecommendations(endPoint, searchRadius);
         } else {
             window.alert('Directions request failed due to ' + status);
         }
@@ -190,32 +51,33 @@ function planRoute() {
 
 function showStreetView(location) {
     pano.setPosition(location);
-    pano.setPov({ heading: 34, pitch: 10 });
+    pano.setVisible(true);
+    map.setStreetView(pano);
 }
 
-function useCurrentLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            let pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
+function showRecommendations(endPoint, searchRadius) {
+    let request = {
+        location: endPoint,
+        radius: searchRadius,
+        type: ['tourist_attraction']
+    };
 
-            map.setCenter(pos);
-            pano.setPosition(pos);
-            document.getElementById('start').value = pos.lat + ',' + pos.lng;
-        }, function() {
-            handleLocationError(true, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, map.getCenter());
-    }
+    placesService.nearbySearch(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            let recommendationsList = document.getElementById('recommendations');
+            recommendationsList.innerHTML = '';
+            results.forEach(function(place) {
+                let li = document.createElement('li');
+                li.textContent = place.name;
+                li.onclick = function() {
+                    document.getElementById('start').value = document.getElementById('end').value;
+                    document.getElementById('end').value = place.name;
+                    planRoute();
+                };
+                recommendationsList.appendChild(li);
+            });
+        } else {
+            recommendationsList.innerHTML = '<li>No recommendations found</li>';
+        }
+    });
 }
-
-function handleLocationError(browserHasGeolocation, pos) {
-    window.alert(browserHasGeolocation ?
-                'Error: The Geolocation service failed.' :
-                'Error: Your browser doesn\'t support geolocation.');
-}
-
